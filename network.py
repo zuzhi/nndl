2fe9d6da805191e362d003634ea95dd3f096c85a
zuzhi <zuzhi.hu@gmail.com>
2021-03-23 14:50:38 +0800

    first commit

---
+++ network.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Network(object):
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                for x, y in zip(sizes[:-1], sizes[1:])]
+
